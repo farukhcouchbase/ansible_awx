9320031b0b150c6512886ecc9d6bbf18f6285288
farukhcouchbase <ritik.lodha.cn@gmail.com>
2025-06-13 00:17:07 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,17 +5,14 @@ sudo mv couch_check72.zip /opt/couchbase/bin/
 sudo mv couch_check76.zip /opt/couchbase/bin/
 sudo mv couch_check_all.sh /opt/couchbase/bin/
 
-# Change to binary directory
-cd /opt/couchbase/bin/ || exit 1
-
 # Unzip the check files
-sudo unzip -o couch_check72.zip
-sudo unzip -o couch_check76.zip
+sudo unzip -o /opt/couchbase/bin/couch_check72.zip
+sudo unzip -o /opt/couchbase/bin/couch_check76.zip
 
 # Make the scripts executable
-sudo chmod +x couch_check72
-sudo chmod +x couch_check76
-sudo chmod +x couch_check_all.sh
+sudo chmod +x /opt/couchbase/bin/couch_check72
+sudo chmod +x /opt/couchbase/bin/couch_check76
+sudo chmod +x /opt/couchbase/bin/couch_check_all.sh
 
 # Set environment variable
 export COUCH_CHECK_PATH=/opt/couchbase/bin
