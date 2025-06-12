73fb62a9c05834c53de1698d81b9d7bcfc473f63
farukhcouchbase <ritik.lodha.cn@gmail.com>
2025-06-13 00:32:36 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -19,4 +19,4 @@ export COUCH_CHECK_PATH=/opt/couchbase/bin
 
 # Run the check script on all .couch.* files in the travel-sample bucket
 cd /opt/couchbase/var/lib/couchbase/data/travel-sample || exit 1
-$COUCH_CHECK_PATH/couch_check_all.sh *.couch.*
+$COUCH_CHECK_PATH/couch_check_all.sh *.couch.* > /home/ubuntu/couch_check_all.txt
